1fd8fd47519c75ba98b50389381931b5ef97bdc0
La Nhat Huy <lanhathuy1082005@gmail.com>
2025-11-14 17:52:56 +0700

    reorganize

--- User/Staff.js
+++
@@ -1,9 +0,0 @@
-import User from "./User.js";
-
-class Staff extends User {
-    constructor(id, name, pass, email, role) {
-        super(id, name, pass, email, role);
-    }
-}
-
-export default Staff;
